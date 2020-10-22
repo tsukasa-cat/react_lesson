bb781afb553815d26a0656172bcdc0d9fca8eb26
tsukasa-cat <engineer.tsukasa@gmail.com>
2020-10-22 17:41:48 +0900

    react resson

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+import reducer from './reducers'
 import './index.css';
-import App from './App';
+
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer, applyMiddleware(thunk))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <EventsIndex />
+  </Provider>,
   document.getElementById('root')
 );
 
